09b0bcf34955045134cb34b5ed3ff7879ee22151
Sung-young <smes289@gmail.com>
2024-03-30 21:05:48 +0900

    assignment5

---
+++ config/blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+
+    path('blog/create', views.create, name='create'),
+    path('blog/assignment', views.assignment, name='assignment'),
+    path('blog/<int:blog_id>', views.detail, name='detail'), 
+]
